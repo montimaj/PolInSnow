fbd23344b58fb2964a1d0f76075ac98f0d15f35b
montimaj <monti.majumdar@gmail.com>
2019-01-18 16:08:35 +0530

    Everything works now!

--- drysnow.py
+++ drysnow.py
@@ -256,7 +256,7 @@ def calc_snow_depth_hybrid(tmat, image_dict, eps=0.4, threshold=10):
             kz_val = 4 * np.pi * np.deg2rad(DEL_THETA) / (WAVELENGTH * np.sin(lia_val))
             snow_depth[idx] = np.abs((np.arctan(tval.imag / tval.real) + 2 * eps * sinc_inv) / kz_val)
             if snow_depth[idx] > threshold:
-                snow_depth[idx] = 0.
+                snow_depth[idx] = 0
             print('At ', idx, 'Snow depth= ', snow_depth[idx])
     np.save('Out/Snow_Depth', snow_depth)
     write_file(snow_depth.copy(), image_dict['LIA'], 'Snow_Depth_Polinsar', is_complex=False)
@@ -285,20 +285,20 @@ def validate_dry_snow(dsd_file, geocoords, nsize=(1, 1)):
     print(min_fsd, max_fsd, mean_fsd, sd_fsd)
 
 
-image_dict = read_images('../../Documents/Clipped_Tifs')
+image_dict = read_images('../THESIS/SnowSAR/Polinsar/Clipped_Tifs')
 print('Images loaded...\n')
 #pol_vec_HV = calc_pol_vec_dict()['HV']
 #s1, s2, ifg = get_interferogram(image_dict)
 print('Starting coherence matrix calculation ...')
-# s1 = np.load('Out/S1.npy')
-# s2=np.load('Out/S2.npy')
-# ifg = np.load('Out/Ifg.npy')
+#s1 = np.load('Out/S1.npy')
+#s2=np.load('Out/S2.npy')
+#ifg = np.load('Out/Ifg.npy')
 #tmat = calc_coherence_mat(s1, s2, ifg, img_file=image_dict['HV'])
 #tmat = calc_ensemble_cohmat(s1, s2, ifg, img_file=image_dict['HV'], wsize=(2, 2))
 tmat = np.load('Out/Coherence_Ensemble.npy')
 print('Calculating snow depth')
-snow_depth = calc_snow_depth_hybrid(tmat, image_dict, eps=0.1, threshold=6)
+snow_depth = calc_snow_depth_hybrid(tmat, image_dict, threshold=7)
 #snow_depth = np.load('Out/Snow_Depth.npy')
-avg_sd = get_ensemble_avg(snow_depth, (5, 5), image_dict['TOPO'], outfile='Avg_SD')
+avg_sd = get_ensemble_avg(snow_depth, (32, 32), image_dict['TOPO'], outfile='Avg_SD')
 validate_dry_snow('Avg_SD.tif', (700089.771, 3581794.5556)) # Dhundi
 #validate_dry_snow('Avg_SD.tif', (706137.95, 3577522.25)) # Kothi
